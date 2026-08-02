210c1693b727113b20854f49523e8654185fbc91
Vishal Chaudhary <vishallakshya2004@gmail.com>
2026-08-03 04:26:01 +0530

    Time: 13 ms (78.02%), Space: 38.8 MB (38.05%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-
         int m = mat.size();
         int n = mat[0].size();
 
-        queue<pair<int,int>> q;
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        queue<pair<pair<int,int>, int>> q;
 
-                if(mat[i][j] == 0)
-                    q.push({i,j});
-                else
-                    mat[i][j] = -1;
-            }
-        }
-
-        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.empty()){
-
-            auto [x,y] = q.front();
-            q.pop();
-
-            for(auto [dx,dy] : dir){
-
-                int nx = x + dx;
-                int ny = y + dy;
-
-                if(nx >= 0 && nx < m && ny >= 0 && ny < n && mat[nx][ny] == -1){
-
-                    mat[nx][ny] = mat[x][y] + 1;
-                    q.push({nx,ny});
+        // Saare 0 queue me daal do
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
 
-        return mat;
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+
+        while(!q.empty()) {
+
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dist = q.front().second;
+            q.pop();
+
+            ans[row][col] = dist;
+
+            for(int i = 0; i < 4; i++) {
+
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if(nrow >= 0 && nrow < m &&
+                   ncol >= 0 && ncol < n &&
+                   !vis[nrow][ncol]) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, dist + 1});
+                }
+            }
+        }
+
+        return ans;
     }
 };
