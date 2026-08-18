7c50d076fd799f8c6080b16e6740b8bbbad7b634
Vishal Chaudhary <vishallakshya2004@gmail.com>
2026-08-18 12:27:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,46 @@
 class Solution {
-	public:	
-	bool dfsCheck(vector<vector<int>> &graph, vector<int> &vis, vector<int> &pathVis,
-	int node) {
-		vis[node] = 1;
-		pathVis[node] = 1;
-		
-		for (auto it : graph[node]) {
-			if (!vis[it]) {
-				if (dfsCheck(graph, vis, pathVis, it))
-					return true;
-			}
-			else if (pathVis[it]) {
-				return true;
-			}
-		}
-		pathVis[node] = 0;
-		return false;
-	}
-	
-	bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
-        int V = numCourses;
-		vector<int> vis(V, 0);
-		vector<int> pathVis(V, 0);
-		vector<vector<int>> graph(V);
-		for (auto x: prerequisites) {
-			int u = x[0];
-			int v = x[1];
-			
-			graph[v].push_back(u);
-		}
-		
-		for (int i = 0; i<V; i++) {
-			if (vis[i] == 0) {
-				if (dfsCheck(graph, vis, pathVis, i))
-					return false;
-			}
-		}
-		
-		return true;
-	}
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> inDegree(n, 0);
+        vector<vector<int>> graph(n);
+
+        for(auto &x: prerequisites){
+            int u = x[0];
+            int v = x[1];
+            graph[v].push_back(u);
+        } 
+
+        for(int i = 0; i<n; i++){
+            for(auto it : graph[i]){
+                inDegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i = 0; i<n; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it: graph[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(inDegree[i]!= 0){
+                return false;
+            }
+        }
+
+        return true;  
+    }
 };
