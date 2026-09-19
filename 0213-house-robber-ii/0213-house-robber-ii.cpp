943d68f768ee5d25049f3bf3069562dd099074ae
Vishal Chaudhary <vishallakshya2004@gmail.com>
2026-09-19 18:09:33 +0530

    Time: 0 ms (100%), Space: 10.9 MB (18.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
-public:
+    int solve(vector<int>& nums, int l, int r) {
+        vector<int> dp(r - l + 1, -1);
 
-    int rob(vector<int>& nums, int l, int r) {
-        int prev2 = 0;
-        int prev1 = 0;
-
-        for (int i = l; i <= r; i++) {
-            int take = nums[i] + prev2;
-            int skip = prev1;
-
-            int curr = max(take, skip);
-
-            prev2 = prev1;
-            prev1 = curr;
+        if (r - l + 1 == 1) {
+            return nums[l];
         }
 
-        return prev1;
+        dp[0] = nums[l];
+        dp[1] = max(nums[l], nums[l + 1]);
+
+        for (int i = 2; i <= r - l; i++) {
+            int take = nums[l + i] + dp[i - 2];
+            int notTake = dp[i - 1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[r - l];
     }
 
+public:
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        if (n == 1)
+        if (n == 1) {
             return nums[0];
+        }
 
-        int case1 = rob(nums, 0, n - 2);
-        int case2 = rob(nums, 1, n - 1);
+        int ans1 = solve(nums, 0, n - 2);
+        int ans2 = solve(nums, 1, n - 1);
 
-        return max(case1, case2);
+        return max(ans1, ans2);
     }
 };
